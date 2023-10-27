42ee919be88f7cdfa09d4430a71b50782c74a7fb
YaeSaraki <ikaraseay@iCloud.com>
2023-10-27 23:06:51 +0800

    AbstractFactory

--- src/AbstractFactoryPattern/AbstractFactory.java
+++ src/AbstractFactoryPattern/AbstractFactory.java
@@ -1,5 +1,18 @@
 package AbstractFactoryPattern;
 
 public class AbstractFactory {
+  public static void main(String[] args) {
+    Factory factory1 = new Factory1();
+    ProductA productA1 = factory1.createProductA();
+    ProductB productB1 = factory1.createProductB();
+    productA1.info();
+    productB1.info();
 
+    Factory factory2 = new Factory2();
+    ProductA productA2 = factory2.createProductA();
+    ProductB productB2 = factory2.createProductB();
+    productA2.info();
+    productB2.info();
+
+  }
 }
